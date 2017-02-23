ad9c4ecf58e8439e37c005ee0d4a6945ab16b372
Sam Gonshaw <sam@gonshaw.com>
2017-02-23 18:17:03 +0000

    Add comments

--- static/itsgonnarain/itsgonnarain.js
+++ static/itsgonnarain/itsgonnarain.js
@@ -1,25 +1,47 @@
-console.log("It's gonna rain");
+// The global audioContext for pumping to the speakers
 let audioContext = new AudioContext();
+// The global buffer that will contain an audio track
 let gAudioBuffer = ''
+// The global loaded boolean that goes true when gAudioBuffer has loaded
 let loaded = false
+// The global nodes for each track (left and right)
 let sourceNode = ['', '']
+// The global boolean to see if a track is currently playing
 let musicPlaying = false;
+// The length of the track that will be set once loaded
 let trackLength = 0;
+
+// The timing ratio between the tracks
 let ratio = 1.002;
+// The time ofset to start the looping within the audio
 let timeStart = 6.8;
+// The time ofset to end the looping within the audio
 let timeEnd = 8.84;
 
+// An enum for left and right
 ChannelEnum = {
 	    LEFT : 0,
 	    RIGHT : 1,
 }
 
+/**
+ * Returns lowest common multiple of two integers
+ * @param {Number} x
+ * @param {Number} y
+ * @return {Number} lcm
+ */
 function lcm(x, y) {
    if ((typeof x !== 'number') || (typeof y !== 'number')) 
     return false;
   return (!x || !y) ? 0 : Math.abs((x * y) / gcd(x, y));
 }
 
+/**
+ * Returns greatest common divisor of two numbers
+ * @param {Number} x
+ * @param {Number} y
+ * @return {Number} gcd
+ */
 function gcd(x, y) {
   x = Math.abs(x);
   y = Math.abs(y);
@@ -31,15 +53,24 @@ function gcd(x, y) {
   return x;
 }
 
+/**
+ * Updates the trackLength global based on ratio, timeEnd and timeStart
+ */
 function setTrackLength() {
 	trackLength = Math.floor((lcm(1000, ratio * 1000) * (timeEnd - timeStart)) / 1000)
 }
 
 
-function startLoop(audioBuffer, pan = ChannelEnum.LEFT, rate = 1) {
+/**
+ * Using the loaded global gAudioBuffer, will create the left or right node in sourceNode
+ * It will then begin playing it on loop using timeStart and timeEnd to cut it
+ * @param {ChannelEnum} the channel (LEFT/RIGHT) to start
+ * @param {Number} A ratio to 1 to speed up/slow down the track
+ */
+function startLoop(pan = ChannelEnum.LEFT, rate = 1) {
   sourceNode[pan] = audioContext.createBufferSource();
   let pannerNode = audioContext.createStereoPanner();
-  sourceNode[pan].buffer = audioBuffer;
+  sourceNode[pan].buffer = gAudioBuffer;
   sourceNode[pan].loop = true;
   sourceNode[pan].loopStart = timeStart;
   sourceNode[pan].loopEnd = timeEnd;
@@ -51,18 +82,33 @@ function startLoop(audioBuffer, pan = ChannelEnum.LEFT, rate = 1) {
   sourceNode[pan].start(0, 6.8);
 }
 
+/**
+ * Converts a number of seconds into a 0:00 minutes:seconds string
+ * @param {Number} Number of seconds
+ * @param {String} The required string
+ */
 function secondsToString(seconds) {
 	minutes = Math.floor(seconds / 60);
 	seconds %= 60; 
 	return minutes + ':' + Math.floor(seconds / 10) + (seconds % 10);
 }
 
+/**
+ * Updates the innerHTML of pPlayback time with the given time into trackLength
+ * @param {Number} time
+ */
 function updateTimer(time) {
 	document.getElementById("pPlaybackTime").innerHTML = 
 		secondsToString(time) + '/' +
 		secondsToString(trackLength);
 }
 
+/**
+ * Increments a static 'tick' and updates the pPlaybackTime
+ * Stops playback if the end of the track is reached
+ * TODO: potentially call more often and beter pinpoint when to stop
+ * Must only be called when music playing
+ */
 function playbackTick() {
 	if (!musicPlaying) {
 		throw {name : "TickError", message : "tick when no music playing"}; 
@@ -79,6 +125,11 @@ function playbackTick() {
 	}
 }
 
+/**
+ * Begins both audio tracks given the loading of gAudioBuffer.
+ * Updates Play button and Stop Button accordingly.
+ * Starts the timer
+ */
 function startMusic() {
       	document.getElementById("btnPlay").disabled = true;
 	startLoop(gAudioBuffer, ChannelEnum.LEFT);
@@ -90,6 +141,11 @@ function startMusic() {
 	playbackTick.interval = setInterval(playbackTick, 1000);
 }
 
+/**
+ * Ends both audio tracks.
+ * Updates Play button and Stop Button accordingly.
+ * Ends the timer
+ */
 function stopMusic() {
       	document.getElementById("btnStop").disabled = true;
 	sourceNode[ChannelEnum.LEFT].stop()
@@ -99,6 +155,11 @@ function stopMusic() {
       	document.getElementById("btnPlay").disabled = false;
 }
 
+/**
+ * Fetches an audio track to gAudioBuffer and updates loaded global when loaded.
+ * Also unmasks play button when loaded
+ * @param {String} URI to fetch
+ */
 function getMusic(audioTrack) {
   fetch(audioTrack)
     .then(response => response.arrayBuffer())
--- views.py
+++ views.py
@@ -1,4 +1,8 @@
 from django.views import generic
 
+
 class IndexView(generic.base.TemplateView):
+    """
+        Simple view to serve the index page.
+    """
     template_name = 'itsgonnarain/index.html'
